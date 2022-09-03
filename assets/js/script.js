0c5abe67c8d6b5659cb1e25a3c417765ba6162a7
aslavinska <anastasija.slavinska@avl.com>
2022-09-03 13:42:18 +0000

    adding function statements to check if the game is over

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,23 +12,46 @@ let catProps = cat.getBoundingClientRect();
 let background = document.querySelector('.background').getBoundingClientRect();
 
 // Getting reference to the score element
-let scoreValue = document.querySelector('.scores');
+let score = document.querySelector('.score'); //could be broken 
 let message = document.querySelector('.message');
 
 // Setting initial game state to start
-let gameInitialState = 'Start';
+let gameState = 'Start';
+
+// Add an eventlistener
+document.addEventListener('keydown', (e) => {
+    
+    // If the enter key is pressed start the game 
+    if (e.key == 'Enter' && gameState != 'Play') {
+      document.querySelectorAll('.pipe_sprite').forEach((e) => {
+        e.remove();
+      });
+      cat.style.top = '55vh';
+      gameInitialState = 'Play';
+      message.innerHTML = '';
+      score.innerHTML = '0';
+      play();
+    }
+  });
 
 function play(){
-
+move();
 }
 function move(){
+    // Check if the game is over
+    if (gameState != 'Play') return;
 
+    // Check all pipe elements
+     let pipeSprite = document.querySelectorAll('.pipe_sprite');
+     pipeSprite.forEach((element)=>{
+
+     })
 }
 
 function gravityFall(){
-
+    if (gameState != 'Play') return;
 }
 
 function callPipe(){
-    
+    if (gameState != 'Play') return;
 }
