31933d5e9ab88d734aa1018849ba23e736ca18e3
aslavinska <anastasija.slavinska@avl.com>
2022-09-03 13:15:37 +0000

    adding variables and consts linking document html with js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+// Background scrolling speed
+let backgroundMovingSpeed = 4;
+
+// Constant Gravity value
+const gravity = 0.5;
+
+// Getting the cat element
+let cat = document.querySelector('.cat');
+
+// Getting the cat element properties
+let catProps = cat.getBoundingClientRect();
+let background = document.querySelector('.background').getBoundingClientRect();
+
+// Getting reference to the score element
+let scoreValue = document.querySelector('.scores');
+let message = document.querySelector('.message');
+
+// Setting initial game state to start
+let gameInitialState = 'Start';
+
 function play(){
 
 }
