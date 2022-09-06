a052f165711cd9b64485c62a487d052fa1eab689
aslavinska <anastasija.slavinska@avl.com>
2022-09-06 09:30:49 +0000

    Adding basic javascript structure with functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 // check if the app is fully loaded and identify user actions
 window.addEventListener('load', ()=>{
     // event listener for each icon item 
+    let userChoice ='';
+    document.querySelectorAll('user-choice .game-card').forEach(card=>{
+        card.addEventListener('click',(event)=>{
+            userChoice = getUserChoice(event);
+            startGame();
+        })
+    })
+ 
 })
+
+function startGame(){
+
+}
+
+function getUserChoice(event){
+
+}
