cd214679b20f8b8bc768b5d2c4ef26842b51d8bf
aslavinska <anastasija.slavinska@avl.com>
2022-09-07 07:57:33 +0000

    Adding rules map and computer choices logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,34 @@
-// check if the app is fully loaded and identify user actions
-window.addEventListener('load', ()=>{
-    // event listener for each icon item 
+function gameMain(){
     let userChoice ='';
     let computerChoice ='';
-    document.querySelectorAll('.user-choice .game-card').forEach(card=>{
-        card.addEventListener('click',(evt)=>{
-            userChoice = getUserChoice(evt.target);
-            computerChoice = getComputerChoice();
-            
-            startGame();
-        })
+    const cards = ['rock', 'paper', 'scissors', 'cat', 'mouse'];
+    // check if the app is fully loaded and identify user actions
+    window.addEventListener('load', () => {
+        
+        document.querySelectorAll('.user-choice .game-card').forEach(card => {
+            card.addEventListener('click', (evt) => {
+                userChoice = getUserChoice(evt.target);
+                computerChoice = getComputerChoice();
+                console.log(computerChoice);
+                startGame();
+            })
+        });
+    
     })
- 
-})
 
 function startGame(){
 
 }
 
-function getUserChoice(target){
-    console.log(target);
-    if (target.nodeName === 'IMG'){
+function getUserChoice(target) {
+    if (target.nodeName === 'IMG') {
         return target.parentElement.classList[1];
     }
     return target.classList[1];
 }
 
 function getComputerChoice(){
-
+    return cards[Math.floor(Math.random()*5)];
 }
 function getWinner(){
 
@@ -44,4 +45,5 @@ function tryAgain(){
 
 function clearResult(){
     
+}
 }
